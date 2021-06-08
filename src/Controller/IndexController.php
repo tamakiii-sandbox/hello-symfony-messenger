4d32f032d1006ba12a30985c04dbb40ee2848751
tamakiii <tamakiii@users.noreply.github.com>
2021-06-08 18:04:08 +0900

    shakei sample

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+use App\Object\RequestNotification;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    public function __construct(
+        private MessageBusInterface $bus,
+    ) {}
+
+    #[Route('/')]
+    public function index(Request $request): Response
+    {
+        $type = $request->request->getInt('type');
+        $notification = new RequestNotification($type, uniqid());
+        $this->bus->dispatch($notification);
+
+        return new JsonResponse([
+            'messageId' => $notification->getId(),
+            'success' => true
+        ]);
+    }
+}
---
+++ src/Object/RequestNotification.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Object;
+
+class RequestNotification
+{
+    public function __construct(
+        private int $type,
+        private string $id,
+    ) {}
+
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+}
